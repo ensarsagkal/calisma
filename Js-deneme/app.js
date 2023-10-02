dd0df991276b6d67e74592bf1d6f83ab7c524fe7
ensar <ensarsagkal@gmail.com>
2023-10-02 13:14:46 +0300

    js documents added

---
+++ Js-deneme/app.js
@@ -0,0 +1,123 @@
+console.log("Hello_World!");
+
+// ? --------------IF ELSE STATEMENT-------------
+
+// const number = prompt("enter a number:  ")
+// if (number>0){
+//     console.log(`${number} is a positive number`);
+// }
+// else if(number==0){
+//     console.log(`${number} is zero`);
+// }
+// else{
+// console.log(`${number} is a negative number`);}
+
+// let a= prompt("enter a number: ")
+// if(a>0){console.log("number is a positive number");
+// } else{if (a==0){console.log("nuber is zero");
+// }else{console.log("number is negative");}}
+
+// let age = prompt("enter your age as a number: ")
+
+// if(age>=18){console.log("Adult");
+// }else{if(age>=10){console.log("Teenager");
+//         }else if(age>=4){console.log("Child");
+//         }else if(age>0){console.log("Baby");
+//         }else{console.log("Error.Please enter positive number");}}
+
+// ?--------------TERNARY STATEMENT-------------
+// const age =prompt("enter your age: ")
+// let result=(age >=18) ? "legal" : "not legal yet";
+// console.log(`you are ${result}`)
+
+// ?--------------SWICTH CASE STATEMENT-----------
+
+// let day=prompt("_")
+// day =parseInt(day)
+// let dayName;
+
+// switch(day){
+//     case 1:
+//         dayName="Monday";
+//         break;
+//     case 2:
+//         dayName= "Tuesday";
+//         break;
+//     case 3:
+//         dayName="Wednesday";
+//         break;
+//     case 4:
+//         dayName="Thursday";
+//         break;
+//     case 5:
+//         dayName ="Friday";
+//         break;
+//     case 6:
+//         dayName = "Saturday";
+//         break;
+//     case 7 :
+//         dayName = "Sunday";
+//         break;
+//     default:
+//         dayName="Invalid Day Number"
+// }
+// console.log(dayName);
+
+// !-----------ELSE IF---------------
+
+// const age = prompt("enter your age : ")
+// let output
+// if(age>=18){
+//     output = "adult"
+// }else if(age>=15){
+//     output="Teenager"
+// }else if(age>=4){
+//     output = "Child"
+// }else if(age>=0){
+//     output = "Baby"
+// }else{
+//     output = "Invalid input"
+// }
+// console.log(output);
+
+// const number = prompt("enter a number")
+
+// if(number>0){
+//     console.log(`${number} sayisi pozitiftir`);
+// }else{
+//     if(number==0){
+//         console.log(`${number} sayisi sifirdir`);
+//     }else{console.log(`${number} sayisi negatiftir`);}
+// }
+
+// if(number>=0){
+//     if(number==0){console.log("zero");
+// }else{console.log("positive");}
+// }else{console.log("negative");}
+
+// let numb1 = prompt("enter first number: ");
+
+// let numb2 = prompt("enter second number: ");
+
+// let numb3 = prompt("enter third number: ")
+// numb1=parseInt(numb1)
+// numb2=parseInt(numb2)
+// numb3=parseInt(numb3)
+
+// if (numb1 > numb2) {
+//   if (numb1 > numb3) {
+//     console.log(`max number is ${numb1}`);
+//   } else {
+//     console.log(`max number is ${numb3}`);
+//   }
+// } else if (numb2 > numb3) {
+//   console.log(`max number is ${numb2}`);
+  
+// } else {
+//     console.log(`max number is ${numb3}`);
+//   }
+
+// const maxNumber =Math.max(numb1,numb2,numb3)
+// console.log(`max number is ${maxNumber}`);
+
+
