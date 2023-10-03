11490c974aa31bdd90725af9a1732ccb68aba304
ensar <ensarsagkal@gmail.com>
2023-10-03 21:29:05 +0300

    leap year

--- Js-deneme/app.js
+++ Js-deneme/app.js
@@ -499,21 +499,28 @@ console.log("Hello_World!");
 
 
 // ! day and season
-let month =prompt("enter a month: 1/12")
-let day= prompt("enter a day: 1/31")
+// let month =prompt("enter a month: 1/12")
+// let day= prompt("enter a day: 1/31")
 
-if(!(month>=1 &&month<=12)|| !(day>=1 && day<=31)){
-    console.log("Error");
-}else{
-let season
-if((month>=3 && month<=5)||(month==2 && day>=21 && day<=29)){
-    season="spring"
-}else if((month>=6 && month<=8)||(month==9 && day<=21)){
-    season="summer"
-}else if((month>=10 && month<=11)||(month==9 && day>=22)||(month==12 && day<=20)){
-    season="fall"
-}else if((month==1)||(month==12 && day>=21)||(month==12 && day<=20)){
-    season="winter"
-}
-console.log(`The season for ${month} / ${day} is ${season}`);
-}
+// if(!(month>=1 &&month<=12)|| !(day>=1 && day<=31)){
+//     console.log("Error");
+// }else{
+// let season
+// if((month>=3 && month<=5)||(month==2 && day>=21 && day<=29)){
+//     season="spring"
+// }else if((month>=6 && month<=8)||(month==9 && day<=21)){
+//     season="summer"
+// }else if((month>=10 && month<=11)||(month==9 && day>=22)||(month==12 && day<=20)){
+//     season="fall"
+// }else if((month==1)||(month==12 && day>=21)||(month==12 && day<=20)){
+//     season="winter"
+// }
+// console.log(`The season for ${month} / ${day} is ${season}`);
+// }
+
+// !leap year
+// let year = prompt("enter a year")
+
+// if((year % 4 ==0)&& !(year % 100==0)||(year%400==0)){
+//     console.log(`${year} is leap year`);
+// }else{console.log(`${year} is not  leap year`);}
