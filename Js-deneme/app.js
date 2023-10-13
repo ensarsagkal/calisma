e03e7b95f91e7a3c86b26ec7967033dd4a4354cb
ensar <ensarsagkal@gmail.com>
2023-10-13 15:49:03 +0300

    end of work

--- Js-deneme/app.js
+++ Js-deneme/app.js
@@ -1030,3 +1030,39 @@ let str ="30 Days Of JavaScript"
 // console.log('You cannot end a sentence with because because because is a conjunction'.lastIndexOf("because"));
 // console.log('You cannot end a sentence with because because because is a conjunction'.search("because"));
 // console.log(str.trim());
+
+// const str = '30 Days Of 20 JAvaScript'
+
+// console.log(str.startsWith('30'));
+// console.log(str.endsWith('Script'));
+// console.log(str.indexOf('a',));
+// console.log(str.length);
+
+// console.log('30 Days of'.concat(' JavaScript'));
+
+// console.log(str.repeat(3));
+//10.soru
+// let text = "JavaScript"
+// leng = text.length
+// console.log(leng);
+
+// let randomSayi = Math.floor(Math.random()*leng)
+// console.log(randomSayi);
+// console.log(text[randomSayi]);
+
+//!--------------- Level-3 -------------------
+
+// let tex = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+// sayi = tex.match(/love/gi)
+// console.log(sayi.length);
+// console.log(tex.match(/love/gi).length);
+
+
+// const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+
+// newSentence = sentence.replace(/%/gi,"").replace(/@/gi,"").replace(/#/gi,"").replace(/&/gi,"").replace(/\$/gi,"");
+
+// console.log(sentence.replace(/[^a-zA-Z0-9\s]/gi, ""));
+// const cleanedSentence = sentence.replace(/[^a-zA-Z0-9\s]/g, '')
+
+// console.log(newSentence.split(" "));
