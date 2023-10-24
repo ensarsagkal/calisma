42c4791b27de73e239d6d2c82d82c234c90bc471
ensar <ensarsagkal@gmail.com>
2023-10-24 15:49:24 +0300

    filteredbooks json

--- Js-deneme/app.js
+++ Js-deneme/app.js
@@ -1702,24 +1702,52 @@ const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle"
 // const isimler = ["Alice", "Bob", "Charlie", "David", "Eve"]
 // const newİS =isimler.filter((isim)=>isim.length>=5)
 // console.log(newİS);
-const team = [
-    { name: "Ahmet", surname: "Can", job: "Developer", age: 30 },
-    { name: "Mary", surname: "Bary", job: "tester", age: 22 },
-    { name: "Hazel", surname: "Nut", job: "developer", age: 20 },
-  ]
+// const team = [
+//     { name: "Ahmet", surname: "Can", job: "Developer", age: 30 },
+//     { name: "Mary", surname: "Bary", job: "tester", age: 22 },
+//     { name: "Hazel", surname: "Nut", job: "developer", age: 20 },
+//   ]
 
- const names = team.map((p)=>p.name)
- console.log(names);
+//  const names = team.map((p)=>p.name)
+//  console.log(names);
 
-const sumOfAges = team.reduce((s,p)=>s+p.age,0)
-console.log(sumOfAges);
+// const sumOfAges = team.reduce((s,p)=>s+p.age,0)
+// console.log(sumOfAges);
 
-const lowerSurname = team.map((p)=>{
-    return p.surname.toUpperCase()
-})
-console.log(lowerSurname);
+// const lowerSurname = team.map((p)=>{
+//     return p.surname.toUpperCase( )
+// })
+// console.log(lowerSurname);
 
-const newObj = team.map((p)=>{
-    return {fulname:`${p.name.toUpperCase()} ${p.surname.toUpperCase()}`,ageplus : `${p.age+5}` }
-})
-console.log(newObj);
+// const newObj = team.map((p)=>{
+//     return {fulname:`${p.name.toUpperCase()} ${p.surname.toUpperCase()}`,ageplus : `${p.age+5}` }
+// })
+// console.log(newObj);
+
+const books = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', rating: 4.2 },
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', rating: 4.6 },
+    { title: '1984', author: 'George Orwell', rating: 4.0 },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', rating: 4.7 },
+    { title: 'Pride and Prejudice', author: 'Jane Austen', rating: 4.5 },
+    { title: 'Moby-Dick', author: 'Herman Melville', rating: 4.1 },
+    { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', rating: 4.8 },
+    { title: 'The Hobbit', author: 'J.R.R. Tolkien', rating: 4.3 },
+    { title: 'Harry Potter and the Sorcerer\'s Stone', author: 'J.K. Rowling',
+   rating: 4.7 },
+    { title: 'Brave New World', author: 'Aldous Huxley', rating: 4.0 },
+    { title: 'The Shining', author: 'Stephen King', rating: 4.2 },
+    { title: 'The Da Vinci Code', author: 'Dan Brown', rating: 3.9 },
+    { title: 'The Alchemist', author: 'Paulo Coelho', rating: 4.4 },
+    { title: 'War and Peace', author: 'Leo Tolstoy', rating: 4.9 },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', rating: 4.5 },
+    { title: 'The Odyssey', author: 'Homer', rating: 4.6 },
+    { title: 'Frankenstein', author: 'Mary Shelley', rating: 4.2 },
+    { title: 'The Hunger Games', author: 'Suzanne Collins', rating: 4.1 },
+    { title: 'The Road', author: 'Cormac McCarthy', rating: 4.3 },
+    { title: 'The Hitchhiker\'s Guide to the Galaxy', author: 'Douglas Adams',rating: 4.7 },
+    { title: 'The Count of Monte Cristo', author: 'Alexandre Dumas', rating: 4.8 },]
+
+
+    const filteredBooks= books.filter((b)=>b.rating>=4.5)
+    console.log(filteredBooks);
