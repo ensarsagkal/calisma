d138b30dc4c88828b42eacad1bf1382f6e03b88d
ensar <ensarsagkal@gmail.com>
2023-10-31 14:16:20 +0300

    question

--- Js-deneme/app.js
+++ Js-deneme/app.js
@@ -1985,36 +1985,50 @@ const users = [
 
 
 
+//   const sumAll=(...n)=>{
+//     console.log(n);
+//     return n.reduce((sum,num)=>sum+num)
+//   }
+
+// console.log(`sum of all : ${sumAll(5,8,99,7)}`);
+
+
+// const numbers=[2,5,9,4,10,99] 
+// const maxNum= Math.max(...numbers)
+// console.log(maxNum); 
+
+
+
 
 const products = [
-    { name: "Product 1", price: 20, category: "Electronics" },
-    { name: "Product 2", price: 30, category: "Clothes" },
-    { name: "Product 3", price: 40, category: "Electronics" },
-    { name: "Product 4", price: 50, category: "Clothes" },
-    { name: "Product 5", price: 60, category: "Clothes" },
-    { name: "Product 6", price: 70, category: "Electronics" },
-    { name: "Product 7", price: 80, category: "Clothes" },
-    { name: "Product 8", price: 90, category: "Electronics" },
-   ];
-  //  const electronics = products.filter((p)=>p.category==="Electronics")
-  //  const avgEletronics =electronics.reduce((sum,p)=> sum+p,0)/electronics.length
-
-  const calculatAvg=(products)=>{
-    
+  { name: "Product 1", price: 20, category: "Electronics" },
+  { name: "Product 2", price: 30, category: "Clothes" },
+  { name: "Product 3", price: 40, category: "Electronics" },
+  { name: "Product 4", price: 50, category: "Clothes" },
+  { name: "Product 5", price: 60, category: "Clothes" },
+  { name: "Product 6", price: 70, category: "Electronics" },
+  { name: "Product 7", price: 80, category: "Clothes" },
+  { name: "Product 8", price: 90, category: "Electronics" },
+ ];
+ /* Use map to create an object with category as the key
+ and an array of products as the value */
+ const productsByCategory = products.reduce((acc, product) => {
+  const category = product.category;
+  if (!acc[category]) {
+  acc[category] = [];
   }
-
-
-
-
-
-
-
- 
-
-  const sumAll=(...n)=>{
-    console.log(n);
-    return n.reduce((sum,num)=>sum+num)
-  }
-
-
-  console.log(`sum of all : ${sumAll(5,8,99,7)}`);
+  acc[category].push(product);
+  return acc;
+ }, {});
+ console.log(productsByCategory);
+ // Use map to calculate the average price for each category
+ const avgPriceByCategory = Object.keys(productsByCategory).map(category => {
+  const sum = productsByCategory[category].reduce((acc, product) => acc +
+ product.price, 0);
+  return { category: category, average: sum / productsByCategory[category].length
+ };
+ });
+ // Use filter to only select categories with an average above a certain threshold
+ const highPricedCategories = avgPriceByCategory.filter(category => category.average
+ > 50);
+  console.log(highPricedCategories)
