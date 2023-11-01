e777dfef764e6797d961dbd0d4f71e4a046c6dd0
ensar <ensarsagkal@gmail.com>
2023-11-01 12:42:24 +0300

    denemeler

---
+++ Js-deneme/deneme.js
@@ -0,0 +1,9 @@
+let element = document.getElementsByTagName("h2")
+console.log(element);
+let element2 = document.querySelectorAll("h2")
+console.log(element2);
+
+let classSelector= document.querySelector(".section-c").style.backgroundColor="red";
+console.log(classSelector);
+
+
