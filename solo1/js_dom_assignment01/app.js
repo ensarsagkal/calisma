50974aae06e8e7d30b174b79b2e24d03f1d1815b
ensar <ensarsagkal@gmail.com>
2023-11-02 13:48:04 +0300

    step4 done

--- solo1/js_dom_assignment01/app.js
+++ solo1/js_dom_assignment01/app.js
@@ -8,3 +8,19 @@ header.style.textAlign = "center"
 
 // !------step 2--------
 document.getElementById("title").textContent = "JavaScript Dom Assignment 1"
+
+// !------step 3--------
+const navItem = document.querySelector(".nav-item")
+navItem.style.display = "flex"
+navItem.style.justifyContent = "center"
+navItem.style.gap = "1rem"
+navItem.style.listStyleType = "none"
+
+// !------step 4--------
+const userName =document.querySelector("#username")
+userName.value = "Ensar" 
+const password = document.querySelector("#password")
+password.value = "123456"
+password.type ="text"
+
+// !------step 5--------
