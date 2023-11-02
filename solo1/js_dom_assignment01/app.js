ab60b5fae636a7fb61afc77ab47e89a549e2429c
ensar <ensarsagkal@gmail.com>
2023-11-02 13:32:26 +0300

    assingment01

---
+++ solo1/js_dom_assignment01/app.js
@@ -0,0 +1,10 @@
+
+// !------step 1--------
+const header = document.querySelector(".header")
+header.style.backgroundColor = "red"
+header.style.color = "white"
+header.style.padding = "10px"
+header.style.textAlign = "center"
+
+// !------step 2--------
+document.getElementById("title").textContent = "JavaScript Dom Assignment 1"
