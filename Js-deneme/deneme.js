b625db95a3c28eca392a7f0d941cdbe3da0c4f7c
ensar <ensarsagkal@gmail.com>
2023-11-06 16:50:54 +0300

    js pracktise

--- Js-deneme/deneme.js
+++ Js-deneme/deneme.js
@@ -1,9 +1,45 @@
-let element = document.getElementsByTagName("h2")
-console.log(element);
-let element2 = document.querySelectorAll("h2")
-console.log(element2);
+// let element = document.getElementsByTagName("h2")
+// console.log(element);
+// let element2 = document.querySelectorAll("h2")
+// console.log(element2);
 
-let classSelector= document.querySelector(".section-c").style.backgroundColor="red";
-console.log(classSelector);
+// let classSelector= document.querySelector(".section-c").style.backgroundColor="red";
+// console.log(classSelector);
 
 
+// let lion = {
+//     'lion-baby' : "cub"
+//   };
+  
+//   // dot notation
+// //   console.log(lion.lion-baby); // error: ReferenceError: baby is not defined
+//   // bracket notation
+//   console.log(lion['lion-baby']); // "cub"
+
+
+
+
+//   const number = 5;
+//   const gavebirth = true;
+  
+//   let animal = {
+//     name: 'lion',
+//     age: 6,
+//     [gavebirth && 'babies']: number
+//   };
+  
+//   console.log(animal)
+let staffs = [
+    { name: "Susan", age: 14, salary: 100 },
+    { name: "Daniel", age: 16, salary: 120 },
+    { name: "Bruno", age: 56, salary: 400 },
+    { name: "Jacob", age: 15, salary: 110 },
+    { name: "Sam", age: 64, salary: 500 },
+    { name: "Dave", age: 56, salary: 380 },
+    { name: "Neils", age: 65, salary: 540 }
+  ];
+
+
+
+const salary1 = staffs.reduce((sum,p)=>sum + p.salary*1.10,0)
+console.log(salary1);
