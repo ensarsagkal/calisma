430f3bca4dee4e8dc8b80ee52bae5fc70d2b81ff
ensar <ensarsagkal@gmail.com>
2023-11-09 16:41:29 +0300

    addevent

--- solo1/toDo(me)/app.js
+++ solo1/toDo(me)/app.js
@@ -1,1 +1,24 @@
-const addBUtton = document.getElementById("")
+const addButton = document.getElementById("add")
+const clearAllButton = document.getElementById("clear")
+const ul = document.querySelector("ul")
+
+
+
+
+addButton.addEventListener("click",()=> {
+    const li = document.createElement("li")
+    const content = document.createElement("span")
+    const removeButton = document.createElement("button")
+
+    ul.appendChild(li)
+    li.appendChild(content)
+    li.appendChild(removeButton)
+
+    content.textContent=(input.value)
+    removeButton.textContent=("X")
+
+    li.classList.add("li")
+    content.classList.add("added-span")
+    removeButton.classList.add("deleted")
+
+})
