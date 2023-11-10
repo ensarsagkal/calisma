0faf9611483bbaaf81532d17fe22c0245ff242ee
ensar <ensarsagkal@gmail.com>
2023-11-10 16:15:48 +0300

    js last

--- solo1/toDo(me)/app.js
+++ solo1/toDo(me)/app.js
@@ -11,17 +11,21 @@ if(!input.value.trim()){
 }else{
     const li = document.createElement("li")
     const content = document.createElement("span")
+    const todayDate=document.createElement("span")
     const removeButton = document.createElement("button")
 
     ul.appendChild(li)
     li.appendChild(content)
+    li.appendChild(todayDate)
     li.appendChild(removeButton)
 
     content.textContent=(input.value)
+    todayDate.textContent=(new Date().getDay() +"/"+ new Date().getMonth()+"/"+new Date().getFullYear())
     removeButton.textContent=("X")
 
     li.classList.add("li")
     content.classList.add("added-span")
+    todayDate.classList.add("today-date")
     removeButton.classList.add("deleted")
 
     input.value = ""
@@ -37,3 +41,22 @@ clearAllButton.addEventListener("click", () => {
         }
     }
 });
+
+document.addEventListener("keyup",(e)=>{
+    if(e.key ==="Enter"){
+        addButton.click()
+    }else if(e.key==="Escape"){
+        clearAllButton.click()
+    }
+})
+
+
+
+ul.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("added-span")){
+        e.target.classList.toggle("span-checked")
+        e.target.nextElementSibling.classList.toggle("today-ended")
+    }else if(e.target.classList.contains("deleted")){
+        e.target.parentElement.remove()
+    }
+})
