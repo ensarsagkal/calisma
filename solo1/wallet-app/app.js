358b7a7d83d8dc2242d92b0ea03a84eba2514db6
ensar <ensarsagkal@gmail.com>
2023-11-11 21:57:40 +0300

    function added

--- solo1/wallet-app/app.js
+++ solo1/wallet-app/app.js
@@ -4,6 +4,7 @@ const harcamaAlani = document.getElementById("harcamaAlani")
 const kaydet= document.getElementById("kaydet")
 const ekle = document.getElementById("ekle")
 const gelirInput = document.getElementById("gelirInput")
+const kalan = document.getElementById("kalan")
 
 kaydet.addEventListener("click",()=>{
     if(!(tarih.value.trim() &&harcamaAlani.value.trim() && harcamaMiktari.value.trim() )){alert("Eksik alan girdiniz")
@@ -13,7 +14,8 @@ kaydet.addEventListener("click",()=>{
     const tr = tbody.insertRow()
      const tarihtD = tr.insertCell(0)  
      const harcamaTd = tr.insertCell(1)  
-     const miktarTd = tr.insertCell(2)  
+     const miktarTd = tr.insertCell(2)
+     miktarTd.id = 'data'  
      const islemTd = tr.insertCell(3) 
      tarihtD.textContent = tarih.value 
      harcamaTd.textContent = harcamaAlani.value 
@@ -22,13 +24,14 @@ kaydet.addEventListener("click",()=>{
      tarih.value = ""
      harcamaAlani.value = ""
      harcamaMiktari.value = ""
-     
+     budget()
     }
 })
 document.querySelector("tbody").addEventListener("click", (event)=>{
 
     if(event.target.classList.contains('fa-trash')){
         event.target.closest("tr").remove()
+        budget()
     }
        
 })
@@ -36,4 +39,135 @@ document.querySelector("tbody").addEventListener("click", (event)=>{
 ekle.addEventListener("click",()=>{
     document.getElementById("gelir").textContent = gelirInput.value
 })
+
+
+const budget = ()=>{
+    const gider = document.getElementById('gider')
+    const miktar = document.querySelectorAll('#data')
+ 
+    const totalGider = [...miktar].reduce((sum,m)=> sum + Number(m.textContent),0)
+    gider.textContent =totalGider
+    
+
+}
+
+kalan.textContent = gelir.Textcontent - gider.textContent
+
+
+
+
+
+
+
+
+
+
+
+// const tarih = document.getElementById('tarih')
+
+// const harcamaMiktari = document.getElementById('harcamaMiktari')
+
+// const harcamaAlani = document.getElementById('harcamaAlani')
+
+// const kaydet = document.getElementById('kaydet')
+
+
+
+
+
+
+// kaydet.addEventListener('click',() =>{
+// if(!(tarih.value && harcamaAlani.value &&harcamaMiktari.value )){
+//     alert('eksik bilgi girdiniz')
+// }
+// else {
+
+//     const tbody = document.getElementById('table1')
+
+//     //    const tr = document.createElement('tr')
+//     //    tbody.appendChild(tr)
+    
+//     const tr = tbody.insertRow()
+//     const tarihTd = tr.insertCell(0)
+//     const harcamaTd = tr.insertCell(1)
+//     const miktarTd = tr.insertCell(2)
+//      miktarTd.id = 'data'
+//     const islemTd = tr.insertCell(3)
+    
+//     tarihTd.textContent = tarih.value ;
+//     harcamaTd.textContent = harcamaAlani.value;
+//     miktarTd.textContent = harcamaMiktari.value;
+//     islemTd.innerHTML = '<i class="fa-solid fa-trash text-danger d-block text-center"></i>'
+    
+//     // const giderMiktarı = harcamaMiktari.value
+//     // console.log(giderMiktarı);
+
+ 
+// // const giderTotal = document.getElementById('gider')
+
+// // giderTotal.textContent = arrGider.reduce((sum,x)=>sum+Number(x),0)
+
+//     tarih.value = ''
+//     harcamaAlani.value = ''
+//     harcamaMiktari.value = ''
+// }
+
+// budget()
+// localStorage.setItem('bütce',tbody.innerHTML)
+
+// })
+
+
+
+
+// document.querySelector("tbody").addEventListener("click", (event)=>{
+
+//     if(event.target.classList.contains('fa-trash')){
+//         event.target.closest("tr").remove()
+//     }
+
+//     budget()
+//     localStorage.setItem('bütce',tbody.innerHTML)
+    
+// })
+
+
+// const ekle = document.getElementById('ekle')
+// const gelirInput = document.getElementById('gelirInput')
+
+
+
+// const budget = ()=>{
+
+//     ekle.addEventListener('click',()=>{
+
+//         document.getElementById('gelir').textContent = gelirInput.value
+//         localStorage.setItem('bütce',tbody.innerHTML)
+       
+//     })
+    
+
+//     const gider = document.getElementById('gider')
+//     const miktar = document.querySelectorAll('#data')
+ 
+//     const totalGider = [...miktar].reduce((sum,m)=> sum + Number(m.textContent),0)
+//     gider.textContent =totalGider
+    
+//     //? kalanı bulma
+
+//     const kalan = document.getElementById('kalan')
+//     kalan.textContent = Number(gelirInput.value) - Number(gider.textContent )
+
+//     // tbody.innerHTML = localStorage.getItem('bütce')
+
+// }
+
+
+
+
+
+
+
+
+
 
