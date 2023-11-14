19f685c409b1ab8d8f9015772636e2c5dc8e2536
ensar <ensarsagkal@gmail.com>
2023-11-14 21:42:17 +0300

    str

--- solo1/password_generator/app.js
+++ solo1/password_generator/app.js
@@ -1,1 +1,9 @@
 const passArea = document.querySelector(".passArea")
+
+
+// const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//     const numbers = []
+//     const symbols = ["!","@","#","$","%","^","&","*","(",")","\","+",~","|","}","{","[","]",":",";","?",">","<",",",".","/","-","="]
+//     const symbol = []
+//     const letter = []
+//     let pass = []
