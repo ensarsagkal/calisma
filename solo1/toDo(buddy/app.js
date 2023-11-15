8e8ea535aa49d4d0dd3ed7e08b39f5c090849ec9
ensar <ensarsagkal@gmail.com>
2023-11-15 14:47:44 +0300

    taskcounter

--- solo1/toDo(buddy/app.js
+++ solo1/toDo(buddy/app.js
@@ -29,23 +29,42 @@ addBtn.addEventListener("click",()=>{
     li.classList.add("liJs")
     ul.appendChild(li)
     input.value=""
+    totalTaskCounter()
 })
 
 ul.addEventListener("click",(e)=>{
     if(e.target.classList.contains("liJs")){
         e.target.classList.toggle("liJs-checked")
+        totalTaskCounter()
     }else if(e.target.classList.contains("spanJs")){
         e.target.parentElement.remove()
+        totalTaskCounter()
     }
+    
 })
 
 clearBtn.addEventListener("click",()=>{
     while(ul.firstChild){
         ul.removeChild(ul.firstChild)
+        totalTaskCounter()
+
     }
        
     
 })
 
+
+const totalTaskCounter=()=>{
+    const totalTask= document.querySelectorAll(".liJs").length
+    const doneTask = document.querySelectorAll(".liJs-checked").length
+    const result = document.querySelector(".result")
+    result.textContent= `${totalTask} den ${doneTask}'i yapıldı`
+    result.classList.add("counterJs")
+    if(!totalTask){
+        result.style.display ="none"
+    }else{
+        result.style.display ="block"
+    }
+}
 
 
