07c2e91b6a3f3b625d3edf31719487322f39c579
ensar <ensarsagkal@gmail.com>
2023-11-15 14:32:17 +0300

    clear btn

--- solo1/toDo(buddy/app.js
+++ solo1/toDo(buddy/app.js
@@ -3,6 +3,7 @@ const addBtn = document.getElementById("add")
 const clearBtn = document.getElementById("clear")
 const ul = document.querySelector("ul")
 
+
 // addBtn.addEventListener("click",()=>{
 //     const li = document.createElement("li")
 //     const spanX = document.createElement("span")
@@ -27,5 +28,24 @@ addBtn.addEventListener("click",()=>{
     li.appendChild(spanX)
     li.classList.add("liJs")
     ul.appendChild(li)
+    input.value=""
 })
+
+ul.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("liJs")){
+        e.target.classList.toggle("liJs-checked")
+    }else if(e.target.classList.contains("spanJs")){
+        e.target.parentElement.remove()
+    }
+})
+
+clearBtn.addEventListener("click",()=>{
+    while(ul.firstChild){
+        ul.removeChild(ul.firstChild)
+    }
+       
+    
+})
+
+
 
