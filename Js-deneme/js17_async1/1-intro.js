4544ca85f8c438fd80adde50ff46bc3cb16778b5
ensar <ensarsagkal@gmail.com>
2023-11-15 21:31:20 +0300

    session

---
+++ Js-deneme/js17_async1/1-intro.js
@@ -0,0 +1,29 @@
+//*==================================================
+//*        1- Senkron-Asenkron Programlama
+//*==================================================
+
+//! Javascript, single-threaded ve Asenkron Programlama dilidir.
+
+//? Asenkron Programlama
+//? --------------------------------------------------------------
+//? Asenkron Programlama, bir alt gorevin uygulamanin asil thread'inden
+//? bagimsiz olarak arka planda calistirilmasina izin veren paralel programlama
+//? teknigidir. Bu alt gorev tamamlandiginda (basriyla veya basarisizlikla)
+//? asil thread bu konuda bilgilendirilir. Asenkron programlama, uygulamalarin
+//? performansininin artirilmasina ve daha iyi kullanici deneyimine katki saglamaktadir.
+
+//? Ozellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri,
+//? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Asyn Programlama
+//? kullanilmasi cok onemlidir.
+
+//* Senkron
+//* ------------------------------------------------
+
+
+
+
+
+console.log("*** Senkron *** ")
+const timeoutId = setTimeout(()=>{
+    console.log("sure doldu");
+},2000)
