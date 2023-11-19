b2e13a1400e2835e6c6a6a2181c7df3664a0f43a
ensar <ensarsagkal@gmail.com>
2023-11-19 20:52:20 +0300

    recap

--- solo1/flag-app/app1.js
+++ solo1/flag-app/app1.js
@@ -1,6 +1,6 @@
 const searchInput = document.querySelector("#search");
 const selectBar = document.querySelector("#select-bar");
-const countries = document.querySelector(".countries");
+const countriesDiv = document.getElementById("countriesDiv")
 
 
 const getApi = async ()=>{
@@ -9,8 +9,8 @@ const getApi = async ()=>{
         if (res.ok){
             console.log(res);
             const data = await  res.json()
-            console.log(data);
-            // showData(data)
+            allDatas= data
+            showData(data)
             selectBarOptions(data)
           
             
@@ -24,13 +24,63 @@ getApi()
 
 
 let countriesArr=[]
-
+let allDatas = []
 const selectBarOptions= (country)=>{
     //& country.forEach((c)=> countriesArr.push(c.name.common))
     countriesArr = country.map((c)=>c.name.common)
+
+    countriesArr.forEach((country)=>  selectBar.innerHTML += `<option>${country}</option>`)
+ }
+
+ const showData=(country)=>{
+    const {name:{common}, flags:{png},region,capital,languages,currencies,population,borders,maps:{googleMaps}}= country[Math.floor(Math.random()*country.length)]
     
-    console.log(countriesArr);
+   
 
-  
+    countriesDiv.innerHTML = `
 
-}
+
+
+    <div class="card shadow-lg" style="width: 22rem">
+            <img src="${png}" class="card-img-top shadow" alt="..." />
+            <div >
+              <h5 class="p-2 text-center">${common}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <i class="fa-solid fa-earth-oceania"></i><span class="fw-bold"> Region:</span> ${region}
+              </li>
+              <li class="list-group-item">
+                <i class="fas fa-lg fa-landmark"></i>
+                <span class="fw-bold"> Capitals:</span> ${capital}
+              </li>
+              <li class="list-group-item">
+                <i class="fas fa-lg fa-comments"></i>
+                <span class="fw-bold"> Languages:</span> ${Object.values(languages)}
+              </li>
+              <li class="list-group-item">
+                <i class="fas fa-lg fa-money-bill-wave"></i>
+                <span class="fw-bold"> Currencies:</span> ${currencies[Object.keys(currencies)].name} , ${currencies[Object.keys(currencies)].symbol}
+              </li>
+              <li class="list-group-item">
+              <i class="fa-solid fa-people-group"></i></i>
+              <span class="fw-bold"> Population:</span> ${population}
+            </li>
+              <li class="list-group-item">
+              <i class="fa-sharp fa-solid fa-road-barrier"></i>
+              <span class="fw-bold"> Borders:</span>  ${borders || "Komşusu yoktur." }
+            </li>
+            </li>
+            <li class="list-group-item">
+              <i class="fa-solid fa-map-location-dot"></i><span class="fw-bold"> Map:</span> <a href="${googleMaps}" target='_blank'> Go to google map</a> </li>
+            </ul>
+          </div>
+  `
+ }
+
+selectBar.addEventListener("click",(e)=>{
+    const value = selectBar.value
+    const searchedData = allDatas.filter((c)=>value==c.name.common)
+  showData(searchedData)
+
+})
