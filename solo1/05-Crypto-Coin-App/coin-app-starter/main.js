4e75b7b7beb63c3193d0f8fe8db4393ff79f2ada
ensar <ensarsagkal@gmail.com>
2023-11-20 12:17:43 +0300

    submitfilter

--- solo1/05-Crypto-Coin-App/coin-app-starter/main.js
+++ solo1/05-Crypto-Coin-App/coin-app-starter/main.js
@@ -1,2 +1,104 @@
 //? MAIN.JS
 console.log("***** main *****")
+
+// https://api.coinranking.com/v2/coins/
+const searchInput = document.getElementById("searchInput")
+const searchButton = document.getElementById("search")
+const coinList = document.getElementById("coinList")
+const form = document.getElementById("form")
+
+const getCoinApi = async ()=>{
+   coinList.innerHTML=""
+    try {
+        const res= await fetch("https://api.coinranking.com/v2/coins/")
+        // console.log(res);
+        const data = await res.json()
+        // console.log(data);
+        
+        const coins =  data.data.coins
+        allCoinsArr = coins
+       getCoins(coins)
+        // console.log(coins);
+        // console.log(allCoinsArr);
+    } catch (error) {
+        
+    }
+}
+
+getCoinApi()
+
+let allCoinsArr= []
+console.log(allCoinsArr);
+const getCoins= (coin)=>{
+
+coin.forEach(item => {
+    
+
+    const {name, color,symbol,price,iconUrl,change} = item
+
+coinList.innerHTML +=` <li class="coinClass">
+<article class='article1'>
+    <p class="name">${name}</p>
+    <p class="symbol" style="color: ${color}">${symbol}</p>
+</article>
+<p class="price">$ ${price}</p>
+<div class="img svg-icon" style="background-image: url('${iconUrl}')"></div>
+<article class='change'><i class="fa-solid fa-chart-line"></i>
+    <p class="change">${change}</p>
+</article>
+</li>
+
+`
+});   
+}
+form.addEventListener("submit",(e)=>{
+    coinList.innerHTML=""
+    e.preventDefault()
+    const value=searchInput.value
+    const filteredValue = allCoinsArr.filter((c)=>c.name.toLowerCase().includes(value.toLowerCase()))
+    // console.log(allCoinsArr);
+    // console.log(filteredValue);
+    getCoins(filteredValue)
+//    filteredValue.forEach((item)=>{
+       
+//     const {name, color,symbol,price,iconUrl,change} = item
+
+// coinList.innerHTML +=` <li class="coinClass">
+// <article class='article1'>
+//     <p class="name">${name}</p>
+//     <p class="symbol" style="color: ${color}">${symbol}</p>
+// </article>
+// <p class="price">$ ${price}</p>
+// <div class="img svg-icon" style="background-image: url('${iconUrl}')"></div>
+// <article class='change'><i class="fa-solid fa-chart-line"></i>
+//     <p class="change">${change}</p>
+// </article>
+// </li>
+
+// `
+   
+// })
+
+    
+})
+
+
+
+
+// const { name, symbol, price, iconUrl, change, color } = coin
+//     coinList.innerHTML += `
+//         <li class="coinClass">
+//             <article class='article1'>
+//                 <p class="name">${name}</p>
+//                 <p class="symbol" style="color: ${color}">${symbol}</p>
+//             </article>
+//             <p class="price">$ ${price}</p>
+//             <div class="img svg-icon" style="background-image: url('${iconUrl}')"></div>
+//             <article class='change'><i class="fa-solid fa-chart-line"></i>
+//                 <p class="change">${change}</p>
+//             </article>
+//         </li>`;
+//     const div = document.querySelector('.change');
+//     if (change) {
+//         change >= 0 ? div.classList.add('green') : div.classList.add('red');
+//     }
