80ce46cc8a7cb5f378a3cae11d54a673325cf7b1
ensar <ensarsagkal@gmail.com>
2023-11-22 22:49:09 +0300

    coinClass

--- solo1/coinApp-noah/coin-app-inclass/src/renderData.js
+++ solo1/coinApp-noah/coin-app-inclass/src/renderData.js
@@ -3,11 +3,16 @@ export const renderCoins=(coin)=>{
     const {price,name,iconUrl,change,symbol,rank}= coin
     const coinsUl = document.querySelector("main .coins")
     const newCoinLi = document.createElement("li")
+    newCoinLi.classList.add("coin")
     newCoinLi.innerHTML=`
     <div class="remove-icon">
         <i class="fas fa-window-close"></i>
     
     </div>
+    <h2>
+        <span>${name}</span>
+        <sup>${symbol}</sup>
+    </h2>
 
     
     
