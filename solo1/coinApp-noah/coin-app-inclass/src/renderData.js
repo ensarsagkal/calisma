acd8b91110b5a6d125c1dfe45369293db45970e8
ensar <ensarsagkal@gmail.com>
2023-11-22 23:36:58 +0300

    npm run build

--- solo1/coinApp-noah/coin-app-inclass/src/renderData.js
+++ solo1/coinApp-noah/coin-app-inclass/src/renderData.js
@@ -1,35 +1,33 @@
-export const renderCoins=(coin)=>{
-    // console.log(coin);
-    const {price,name,iconUrl,change,symbol,rank}= coin
+export const renderCoins = (coin) => {
+    const { price, name, iconUrl, change, symbol, rank } = coin
     const coinsUl = document.querySelector("main .coins")
+  
+    console.log(change)
     const newCoinLi = document.createElement("li")
     newCoinLi.classList.add("coin")
-    newCoinLi.innerHTML=`
-    <div class="remove-icon">
-        <i class="fas fa-window-close"></i>
-    
-    </div>
-    <h2>
-        <span>${name}</span>
-        <sup>${symbol}</sup>
-    </h2>
-    <div class="coin-temp>$${Number(price).toFixed(4)}</div>
-    <figure>
-        <img src="${iconUrl}" alt=""/>
-        <figcaption style='color:${change < 0 ? "red" : "green"}'>
-            <i class="fa-solid fa-chart-line"></i>
-            <span>${change || "0"}%</span>
-            
-        </figcaption>
-        <div>RANK : ${rank}</div>
-    </figure>
-
-    
-    
+    newCoinLi.innerHTML = `
+      <div class="remove-icon">
+          <i class="fas fa-window-close"></i>
+      </div>
+      <h2 class="coin-name">
+          <span>${name}</span>
+          <sup>${symbol}</sup>
+      </h2>
+      <div class="coin-temp">$${Number(price).toFixed(4)}</div>
+      <figure>
+          <img src="${iconUrl}" alt="" />
+          <figcaption style='color:${change < 0 ? "red" : "green"}'>
+              <i class="fa-solid fa-chart-line"></i>
+              <span>${change || "0"}%</span>
+          </figcaption>
+          <div>RANK:${rank}</div>
+      </figure>
     `
+    //? oluşan li elementini ul listesine bagladik
     coinsUl.append(newCoinLi)
-
-    newCoinLi.querySelector(".remove-icon").addEventListener("click",()=>{
-        newCoinLi.remove()
+  
+    //? Sil butonu için event tanimlanmasi
+    newCoinLi.querySelector(".remove-icon").addEventListener("click", () => {
+      newCoinLi.remove()
     })
-}
+  }
