2082467d7be0a15d753be177eb7d5aeb3ac7c299
ensar <ensarsagkal@gmail.com>
2023-11-22 13:59:32 +0300

    fetchCats func

---
+++ solo1/cat-img/app.js
@@ -0,0 +1,44 @@
+const loading = document.getElementById("loading");
+const cardDiv= document.getElementById("cardDiv")
+const btn= document.querySelector(".btn")
+
+const loadingTimeOut = setTimeout(() => {
+    loading.style.display = "none";
+    fetchCats(); // fetchCats fonksiyonunu burada çağırıyoruz
+}, 3000);
+
+const fetchCats= ()=>{
+fetch("https://api.thecatapi.com/v1/images/search?limit=10").then((res)=>{
+    if(!res.ok){
+        throw new Error(cardDiv.innerHTML=`<img src="./img/error.gif" />`)
+    }
+    return res.json()
+
+}).then((data)=>{
+    console.log(data);
+    catDatas=data
+    cardDiv.innerHTML = ''; // cardDiv'i temizliyoruz
+    show(data)
+}).catch((error)=>error)
+
+}
+let catDatas = []
+
+const show=(images)=>{
+    
+    images.forEach((img)=>{
+        const {url}= img
+        cardDiv.innerHTML += `
+        <div class="col-12 col-sm-6 col-lg-4">
+      <div style="height:200px;">
+        <img src="${url}" class="w-100 h-100" alt="...">
+      </div>
+</div>
+        `
+    })
+
+}
+
+btn.addEventListener("click",()=>{
+   fetchCats()
+})
