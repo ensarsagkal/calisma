60781a60182591d5d4b6fcd3f4fe6a4b16ac6be0
ensar <ensarsagkal@gmail.com>
2023-11-24 14:14:13 +0300

    apiRequest done

--- solo1/M1-shopping-project/main.js
+++ solo1/M1-shopping-project/main.js
@@ -1,2 +1,23 @@
-import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { dataToUi } from "./src/dataToUi";
+
+
+const getApiData = async () => {
+  const res = await fetch(`https://anthonyfs.pythonanywhere.com/api/products/`);
+//   console.log(res);
+  try {
+    if (!res.ok) {
+      throw new Error(
+        alert("Opss...Something went wrong.Please try again later ")
+      );
+    } else {
+        const data =await res.json()
+        // console.log(data); 
+        dataToUi(data)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+getApiData();
---
+++ solo1/M1-shopping-project/src/dataToUi.js
@@ -0,0 +1,40 @@
+
+
+ export const dataToUi=(data)=>{
+    const products = document.getElementById("products")
+    console.log(products);
+    data.forEach((item)=>{
+        const {title,description,category,price,image,id}=item
+       products.innerHTML +=`
+       <div class="col">
+       <div class="card">
+         <img
+           src=${image}
+           class="p-2"
+           height="250px"
+           alt="..."
+         />
+         <div class="card-body">
+           <h5 class="card-title line-clamp-1">${title}</h5>
+           <p class="card-text line-clamp-3">${description}</p>
+         </div>
+         <div
+           class="card-footer w-100 fw-bold d-flex justify-content-between gap-3"
+         >
+           <span>Price:</span><span>${price} $</span>
+         </div>
+         <div class="card-footer w-100 d-flex justify-content-center gap-3">
+           <button class="btn btn-danger">Sepete Ekle</button>
+           <button
+             class="btn btn-primary"
+             data-bs-toggle="modal"
+             data-bs-target="#exampleModal"
+           >
+             See Details
+           </button>
+         </div>
+       </div>
+     </div>
+       `
+    })
+}
