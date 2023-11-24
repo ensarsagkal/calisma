f7c631ac92fc0c501653155064e880981ce6705f
ensar <ensarsagkal@gmail.com>
2023-11-24 17:53:34 +0300

    category section added as dynamic

--- solo1/M1-shopping-project/src/categoryArr.js
+++ solo1/M1-shopping-project/src/categoryArr.js
@@ -5,12 +5,28 @@
 export const categoryArrFunc = (data)=>{
    
   const categoryArr=data.reduce((acc,product)=>{
-    if(!product.category){
-        categoryArr.push(product.category)
+    if(!acc.includes(product.category)){
+        acc.push(product.category)
     }
+    // console.log(acc);
+    return acc
   },[])
-  console.log(categoryArr);
-  return categoryArr
 
+ categoryArr.unshift("All")
+ console.log(categoryArr);
+
+
+
+ 
+ const btns= document.getElementById("btns")
+ categoryArr.forEach((categorName)=>{
+    btns.innerHTML+=`
+    <button class="btn btn-warning">${categorName}</button>
+    `
+ })
+
+return categoryArr
 
 }
+
+
